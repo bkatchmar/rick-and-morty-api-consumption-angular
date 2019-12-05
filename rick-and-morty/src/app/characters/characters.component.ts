a3339e8cba36d24de9ff99cf6d05ec754cf604f6
Brian Katchmar <bkatchmar@gmail.com>
2019-12-05 16:52:21 -0500

    Adding in some paging elements

--- rick-and-morty/src/app/characters/characters.component.ts
+++ rick-and-morty/src/app/characters/characters.component.ts
@@ -9,16 +9,29 @@ import { CharactersService } from "../characters.service";
 })
 export class CharactersComponent implements OnInit {
   characterCall: CharacterApiResponse;
+  pages: number[];
+  currentPage = 1;
 
   constructor(private charactersService: CharactersService) { }
 
-  ngOnInit() {
-    this.getCharacters();
-  }
+  ngOnInit() { this.getCharacters(); }
   
   getCharacters(): void {
     this.charactersService.getCharacters().subscribe(characters => {
       this.characterCall = characters;
+      this.fillInPageArray(characters.info.pages);
     });
+  }
+
+  fillInPageArray(total: number): void {
+    this.pages = [] as number[];
+
+    for(var counter:number = 1; counter<=total; counter++) {
+      this.pages.push(counter);
+    }
+  }
+
+  changePage(page: number) : void {
+    this.currentPage = page;
   }
 }
