224e3ff3706c9e55afd0ddb695f32b4191f3da12
Brian Katchmar <bkatchmar@gmail.com>
2019-12-05 15:27:33 -0500

    Style fix

--- rick-and-morty/src/app/characters.service.ts
+++ rick-and-morty/src/app/characters.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { CharacterApiResponse } from "./api_responses/characterapiresponse";
 import { Observable } from "rxjs";
