2ea60370241c3f6cc3bc5bbfba6f50de5f238849
Brian Katchmar <bkatchmar@gmail.com>
2019-12-16 11:47:08 -0500

    API Endpoints are now environment variables

--- rick-and-morty/src/app/characters.service.ts
+++ rick-and-morty/src/app/characters.service.ts
@@ -3,18 +3,18 @@ import { Injectable } from "@angular/core";
 import { CharacterApiResponse } from "./api_responses/characterapiresponse";
 import { Character } from "./api_responses/Character";
 import { Observable } from "rxjs";
+import { environment } from "./../environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class CharactersService {
-  private charactersUrl = "https://rickandmortyapi.com/api/character";  // URL to web api
   constructor(private http: HttpClient) { }
   
   getCharacters(page = 1, term = ""): Observable<CharacterApiResponse> {
-    return this.http.get<CharacterApiResponse>(`${this.charactersUrl}/?page=${page}${(term === "") ? "" : "&name=" + term}`);
+    return this.http.get<CharacterApiResponse>(`${environment.charactersUrl}/?page=${page}${(term === "") ? "" : "&name=" + term}`);
   }
   getCharacter(id: string): Observable<Character> {
-    return this.http.get<Character>(`${this.charactersUrl}/${id}`);
+    return this.http.get<Character>(`${environment.charactersUrl}/${id}`);
   }
 }
--- rick-and-morty/src/app/episodes.service.ts
+++ rick-and-morty/src/app/episodes.service.ts
@@ -2,14 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { EpisodeApiResponse } from "./api_responses/episodeapiresponse";
 import { Observable } from "rxjs";
+import { environment } from "./../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpisodesService {
-  private charactersUrl = "https://rickandmortyapi.com/api/episode";  // URL to web api
   constructor(private http: HttpClient) { }
   getEpisodes(page = 1): Observable<EpisodeApiResponse> {
-    return this.http.get<EpisodeApiResponse>(`${this.charactersUrl}/?page=${page}`);
+    return this.http.get<EpisodeApiResponse>(`${environment.episodesUrl}/?page=${page}`);
   }
 }
